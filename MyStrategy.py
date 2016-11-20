51c898c39b375ff968888fe4857c0e8fe3ce73a0
infsega <infsega@gmail.com>
2016-11-21 01:08:27 +0300

    Fast retreat

--- MyStrategy.py
+++ MyStrategy.py
@@ -5,12 +5,16 @@ from model.Wizard import Wizard
 from model.World import World
 from model.LaneType import LaneType
 from model.Faction import Faction
+from model.LivingUnit import LivingUnit
+from model.Building import Building
+from model.Minion import Minion
+from model.MinionType import MinionType
 
 import math
 import random
 
 WAYPOINT_RADIUS = 100.0
-LOW_HP_FACTOR = 0.5
+LOW_HP_FACTOR = 0.25
 
 
 class Point2D:
@@ -35,6 +39,15 @@ class MyStrategy:
         self.current_move = None
         self.waypoints = None
         self.waypoints_by_lane = None
+        self.strafe_line = 0
+        self.strafe_dir = -1
+
+    def setup_strafe(self):
+        if self.strafe_line == 0:
+            self.strafe_line = random.randint(1,6)
+            self.strafe_dir  = -self.strafe_dir
+        self.current_move.strafe_speed = self.strafe_dir * random.uniform(0, self.game.wizard_strafe_speed)
+        self.strafe_line -= 1
 
     def initialize_tick(self, me: Wizard, world: World, game: Game, move: Move):
         self.me = me
@@ -42,7 +55,7 @@ class MyStrategy:
         self.game = game
         self.current_move = move
 
-    def get_nearest_target(self):
+    def get_nearest_target(self) -> LivingUnit:
         targets = self.world.buildings + self.world.wizards + self.world.minions
         nearest_target = None
         nearest_target_distance = None
@@ -55,14 +68,17 @@ class MyStrategy:
             nearest_target_distance = distance
         return nearest_target
 
-    def go_to_unit(self, unit):
-        self.go_to(unit.x, unit.y)
+    def get_unit_free_attack_distance(self, unit):
+        distance = self.me.get_distance_to_unit(unit)
+        attack_distance = self.get_attack_distance(unit)
+        return distance - 1.1 * attack_distance
 
-    def go_to(self, x, y):
-        angle = self.me.get_angle_to(x, y)
-        self.current_move.turn = angle
-        if abs(angle) < self.game.staff_sector / 4.0:
-            self.current_move.speed = self.game.wizard_forward_speed
+    def enemy_units(self):
+        targets = self.world.buildings + self.world.wizards + self.world.minions
+        return [unit for unit in targets if unit.faction not in [Faction.NEUTRAL, self.me.faction]]
+
+    def get_free_attack_distance(self):
+        return min([self.get_unit_free_attack_distance(unit) for unit in self.enemy_units()])
 
     def get_next_waypoint(self):
         last_waypoint = self.waypoints[-1]
@@ -136,15 +152,37 @@ class MyStrategy:
             print("BOTTOM")
         self.waypoints = self.waypoints_by_lane[self.lane]
 
+    def get_attack_distance(self, unit):
+        if unit is Wizard:
+            return unit.staff_range
+        elif unit is Building:
+            return unit.attack_range
+        elif unit is Minion:
+            if unit.type == MinionType.ORC_WOODCUTTER:
+                return self.game.orc_woodcutter_attack_range
+            elif unit.type == MinionType.FETISH_BLOWDART:
+                return self.game.fetish_blowdart_attack_range
+        else:
+            return 0
+
+    def go_to_unit(self, unit, force=False):
+        self.go_to(unit.x, unit.y)
+
+    def go_to(self, x, y, force=False):
+        angle = self.me.get_angle_to(x, y)
+        self.current_move.turn = angle
+        if force or (abs(angle) < self.game.staff_sector / 4.0):
+            self.current_move.speed = self.game.wizard_forward_speed
+
     def move(self, me: Wizard, world: World, game: Game, move: Move):
         self.initialize_strategy(me, game)
         self.initialize_tick(me, world, game, move)
 
-        move.strafe_speed = random.choice([game.wizard_strafe_speed, -game.wizard_strafe_speed])
+        self.setup_strafe()
 
         if me.life < me.max_life * LOW_HP_FACTOR:
             print("Retreat")
-            self.go_to_unit(self.get_previous_waypoint())
+            self.go_to_unit(self.get_previous_waypoint(), force=True)
             return
 
         nearest_target = self.get_nearest_target()
@@ -160,6 +198,11 @@ class MyStrategy:
                     move.action = ActionType.MAGIC_MISSILE
                     move.cast_angle = angle
                     move.min_cast_distance = distance - nearest_target.radius + game.magic_missile_radius
+
+                if self.get_free_attack_distance() < 0:
+                    print("Keeping away")
+                    self.go_to_unit(self.get_previous_waypoint(), force=True)
+
                 return
 
         print("Go to next waypoint")
