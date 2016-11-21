fb43327420e7b6f86909683c7a035076cd0f11e1
infsega <infsega@gmail.com>
2016-11-21 19:28:28 +0300

    Retreat fallback

--- MyStrategy.py
+++ MyStrategy.py
@@ -10,6 +10,7 @@ from model.Building import Building
 from model.BuildingType import BuildingType
 from model.Minion import Minion
 from model.MinionType import MinionType
+from model.Message import Message
 
 import math
 import random
@@ -29,6 +30,10 @@ def intersection_point(p, v, w):
 
 def distance_to_segment(p, v, w):
     return p.get_distance_to_unit(intersection_point(p, v, w))
+
+
+def collide(x, y, radius, unit: LivingUnit):
+    return unit.get_distance_to(x, y) < unit.radius + radius
 
 
 class Point2D:
@@ -55,10 +60,11 @@ class MyStrategy:
         self.waypoints_by_lane = None
         self.strafe_line = 0
         self.strafe_dir = -1
+        self.last_waypoint = None
 
     def setup_strafe(self):
         if self.strafe_line == 0:
-            self.strafe_line = random.randint(1, 20)
+            self.strafe_line = random.randint(10, 30)
             self.strafe_dir = -self.strafe_dir
         self.current_move.strafe_speed = self.strafe_dir * random.uniform(0, self.game.wizard_strafe_speed)
         self.strafe_line -= 1
@@ -88,7 +94,8 @@ class MyStrategy:
             segment_distance = distance_to_segment(unit, wp[i], wp[i+1])
             if (min_segment_distance is None) or (min_segment_distance > segment_distance):
                 min_segment_distance = segment_distance
-                unit_distance_on_lane = path_length + wp[i].get_distance_to_unit(intersection_point(unit, wp[i], wp[i+1]))
+                path_section = intersection_point(unit, wp[i], wp[i+1])
+                unit_distance_on_lane = path_length + wp[i].get_distance_to_unit(path_section)
             path_length += wp[i].get_distance_to_unit(wp[i+1])
         return unit_distance_on_lane
 
@@ -117,12 +124,10 @@ class MyStrategy:
     def get_vanguard(self) -> LivingUnit:
         vanguard = None
         vanguard_distance = None
-        print("get_vanguard")
         for ally in self.allies():
             lane, distance = self.get_position(ally)
             if lane not in [None, self.lane]:
                 continue
-            print("  ", lane, distance)
             if (vanguard_distance is None) or (distance > vanguard_distance):
                 vanguard = ally
                 vanguard_distance = distance
@@ -166,7 +171,7 @@ class MyStrategy:
                 return waypoint
         return first_waypoint
 
-    def initialize_strategy(self, me: Wizard, game: Game):
+    def initialize_strategy(self, me: Wizard, game: Game, move: Move):
         if self.game is not None:
             return
         random.seed(game.random_seed)
@@ -176,7 +181,7 @@ class MyStrategy:
                 Point2D(100.0, map_size - 100.0),
                 random.choice([Point2D(600.0, map_size - 200.0), Point2D(200.0, map_size - 600.0)]),
                 Point2D(800.0, map_size - 800.0),
-                Point2D(map_size - 600.0, 600.0)
+                Point2D(map_size - 800.0, 800.0)
             ],
             LaneType.TOP: [
                 Point2D(100.0, map_size - 100.0),
@@ -189,7 +194,7 @@ class MyStrategy:
                 Point2D(map_size * 0.25, 200.0),
                 Point2D(map_size * 0.50, 200.0),
                 Point2D(map_size * 0.75, 200.0),
-                Point2D(map_size - 200.0, 200.0)
+                Point2D(map_size - 600.0, 200.0)
             ],
             LaneType.BOTTOM: [
                 Point2D(100.0, map_size - 100.0),
@@ -202,9 +207,10 @@ class MyStrategy:
                 Point2D(map_size - 200.0, map_size * 0.75),
                 Point2D(map_size - 200.0, map_size * 0.50),
                 Point2D(map_size - 200.0, map_size * 0.25),
-                Point2D(map_size - 200.0, 200.0)
+                Point2D(map_size - 200.0, 600.0)
             ]
         }
+
         if me.id in [1, 2, 6, 7]:
             self.lane = LaneType.TOP
             print("TOP")
@@ -214,6 +220,21 @@ class MyStrategy:
         else:
             self.lane = LaneType.BOTTOM
             print("BOTTOM")
+
+        if me.master:
+            move.messages = [
+                Message(LaneType.TOP, None, None),
+                Message(LaneType.BOTTOM, None, None),
+                Message(LaneType.MIDDLE, None, None),
+                Message(self.lane, None, None)
+            ]
+        else:
+            for msg in me.messages:
+                if msg is None:
+                    continue
+                if msg.lane in [LaneType.TOP, LaneType.MIDDLE, LaneType.BOTTOM]:
+                    self.lane = msg.lane
+
         self.waypoints = self.waypoints_by_lane[self.lane]
 
     def get_attack_distance(self, unit):
@@ -239,27 +260,48 @@ class MyStrategy:
             self.current_move.speed = self.game.wizard_forward_speed
 
     def retreat(self):
+        distance_to_check = self.me.radius * 0.5
+        x = self.me.x + math.cos(self.me.angle) * distance_to_check
+        y = self.me.y - math.sin(self.me.angle) * distance_to_check
+
+        x_locked = (x - self.me.radius < 0) or (x + self.me.radius > self.game.map_size)
+        y_locked = (y - self.me.radius < 0) or (y + self.me.radius > self.game.map_size)
+        if x_locked or y_locked:
+            return False
+
+        for unit in self.world.buildings + self.world.minions + self.world.trees + self.world.wizards:
+            if unit.id != self.me.id:
+                if collide(x, y, self.me.radius, unit):
+                    return False
+
         self.current_move.speed = -self.game.wizard_backward_speed
+        return True
 
     def move(self, me: Wizard, world: World, game: Game, move: Move):
-        self.initialize_strategy(me, game)
+        self.initialize_strategy(me, game, move)
         self.initialize_tick(me, world, game, move)
 
         self.setup_strafe()
 
+        move_forward = True
+
         if me.life < me.max_life * LOW_HP_FACTOR:
-            print("Medic!")
-            self.retreat()
-            return
+            if self.retreat():
+                print("Medic!")
+                return
+            print("Medic needed, but no chance to retreat")
+            move_forward = False
 
         vanguard = self.get_vanguard()
         if vanguard is not None:
             vanguard_distance = self.get_unit_distance_on_lane(self.lane, vanguard)
             my_distance = self.get_unit_distance_on_lane(self.lane, self.me)
             if my_distance + 100 > vanguard_distance:
-                print("Retreat. Vanguard is %s on %f (%f %f). Me is %f (%f %f)" % (type(vanguard), vanguard_distance, vanguard.x, vanguard.y, my_distance, self.me.x, self.me.y))
-                self.retreat()
-                return
+                if self.retreat():
+                    print("There is no vanguard. Retreat.")
+                    return
+                print("There is no vanguard. Cannot retreat")
+                move_forward = False
 
         nearest_target = self.get_nearest_target()
         if nearest_target is not None:
@@ -267,8 +309,6 @@ class MyStrategy:
             if distance <= me.cast_range:
                 angle = me.get_angle_to_unit(nearest_target)
                 move.turn = angle
-                print("Turn to target")
-
                 if abs(angle) < game.staff_sector / 2.0:
                     if distance <= game.staff_range:
                         print("Hard attack")
@@ -280,10 +320,17 @@ class MyStrategy:
                     move.min_cast_distance = distance - nearest_target.radius + game.magic_missile_radius
 
                 if self.get_free_attack_distance() < 0:
-                    print("Under attack!")
-                    self.retreat()
+                    if self.retreat():
+                        print("Under attack! Retreat")
+                        return
+                    else:
+                        print("Under attack! Failed to retreat")
 
                 return
 
-        print("Go to next waypoint")
-        self.go_to_unit(self.get_next_waypoint())
+        if move_forward:
+            next_waypoint = self.get_next_waypoint()
+            if next_waypoint != self.last_waypoint:
+                self.last_waypoint = next_waypoint
+                print("Go to next waypoint")
+            self.go_to_unit(next_waypoint)
